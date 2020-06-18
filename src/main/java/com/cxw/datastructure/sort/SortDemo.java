0f70ebd9e31bdf41df58b80df74a3b9f2494123b
chengxuwei <hbyscxw@163.com>
2020-06-18 08:06:42 +0800

    sort

--- src/main/java/com/cxw/datastructure/sort/SortDemo.java
+++ src/main/java/com/cxw/datastructure/sort/SortDemo.java
@@ -9,13 +9,62 @@ import java.util.Arrays;
  */
 public class SortDemo {
     public static void main(String[] args) {
-        int[] ary = {1,3,2,6,7,8};
-        bubbleSort(ary);
+        int[] ary = {10,1,4,5,100,50,30,20,6,7,8};
+        insertSort(ary);
         System.out.println(Arrays.toString(ary));
     }
 
-    private static void shellSort(int[] ary) {
+    private static void shellChangeSort(int[] ary) {
+        /*
+        //第一轮
+        for (int i = 3; i < ary.length; i++) {
+            for (int j = i-3; j >=0 ; j-=3) {
+                if(ary[j]>ary[j+3]){
+                    swap(ary,j,j+3);
+                }
+            }
+        }
+        //第二轮
+        for (int i = 1; i < ary.length; i++) {
+            for (int j = i-1; j >=0 ; j-=1) {
+                if(ary[j]>ary[j+1]){
+                    swap(ary,j,j+1);
+                }
+            }
+        }
+        */
 
+        for (int step = ary.length/2; step > 0 ; step/=2) {
+            for (int i = step; i < ary.length; i++) {
+                for (int j = i-step; j >=0; j-=step) {
+                    if(ary[j]>ary[j+step]){
+                        swap(ary,j,j+step);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void shellInsertSort(int[] ary) {
+        for (int step = ary.length/2; step > 0 ; step/=2) {
+            for (int i = step; i < ary.length; i++) {
+                int insertValue = ary[i];
+                boolean lt = false;
+                int index = 0;
+                for (int j = i-step; j >=0; j-=step) {
+                    if(ary[j]>insertValue){
+                        ary[j+step] = ary[j];
+                        lt = true;
+                    }else{
+                        index = j+step;
+                        break;
+                    }
+                }
+                if(lt){
+                    ary[index] = insertValue;
+                }
+            }
+        }
     }
     private static void insertSort(int[] ary) {
         for (int i = 1; i < ary.length; i++) {
@@ -35,6 +84,24 @@ public class SortDemo {
                 ary[index] = insertValue;
             }
         }
+    }
+
+    /**
+     * 算法时间复杂度
+     * @param ary
+     */
+    public static void insertSort2(int[] ary){
+        for (int i = 1; i < ary.length; i++) {
+            int index = i-1 ;
+            int insertValue = ary[i];
+            while(index >=0 && ary[index]>insertValue){
+                ary[index+1] = ary[index];
+                index--;
+            }
+            ary[index+1] = insertValue;
+        }
+
+
     }
 
     /**
