1f0ed42448019cc5f104d51e92b540deaac60bcb
chengxuwei <hbyscxw@163.com>
2020-06-19 14:40:25 +0800

    merge排序

--- src/main/java/com/cxw/datastructure/sort/SortDemo.java
+++ src/main/java/com/cxw/datastructure/sort/SortDemo.java
@@ -9,9 +9,118 @@ import java.util.Arrays;
  */
 public class SortDemo {
     public static void main(String[] args) {
-        int[] ary = {10,1,4,5,100,50,30,20,6,7,8};
-        insertSort(ary);
+        int[] ary = {10, 1, 4, 5, 100, 50, 30, 20, 6, 7, 8};
+        mergeSort(ary);
         System.out.println(Arrays.toString(ary));
+    }
+
+    private static void mergeSort(int[] ary) {
+        doMergeSort(ary, 0, ary.length - 1);
+    }
+
+    private static void doMergeSort(int[] ary, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            doMergeSort(ary, start, mid);
+            doMergeSort(ary, mid + 1, end);
+            doMerge(ary, start, mid, end);
+        }
+    }
+
+    private static void doMerge(int[] ary, int start, int mid, int end) {
+        int i = start;
+        int j = mid + 1;
+        int[] temp = new int[end - start + 1];
+        int t = 0;
+        while (i <= mid && j <= end) {
+            if (ary[i] <= ary[j]) {
+                temp[t] = ary[i];
+                i++;
+                t++;
+            } else {
+                temp[t] = ary[j];
+                j++;
+                t++;
+            }
+        }
+
+        while (j <= end) {
+            temp[t] = ary[j];
+            j++;
+            t++;
+        }
+
+        while (i <= mid) {
+            temp[t] = ary[i];
+            i++;
+            t++;
+        }
+        System.arraycopy(temp, 0, ary, start, temp.length);
+    }
+
+    private static void quickSort(int[] ary) {
+        doQuickSort2(ary, 0, ary.length - 1);
+    }
+
+    private static void doQuickSort2(int[] ary, int left, int right) {
+        if (left > right) {
+            return;
+        }
+        int l = left;
+        int r = right;
+        int minValue = ary[(l + r) / 2];
+        while (l < r) {
+            while (ary[l] < minValue) {
+                l++;
+            }
+            while (ary[r] > minValue) {
+                r--;
+            }
+            if (ary[l] > ary[r]) {
+                swap(ary, l, r);
+            }
+        }
+        doQuickSort2(ary, left, l - 1);
+        doQuickSort2(ary, l + 1, right);
+    }
+
+
+    private static void doQuickSort(int[] ary, int left, int right) {
+        int l = left;
+        int r = right;
+        int midValue = ary[(l + r) / 2];
+        while (l < r) {
+            while (ary[l] < midValue) {
+                l++;
+            }
+            while (ary[r] > midValue) {
+                r--;
+            }
+            if (l >= r) {
+                break;
+            }
+            swap(ary, l, r);
+            if (ary[l] == midValue) {
+                r--;
+            }
+            if (ary[r] == midValue) {
+                l++;
+            }
+
+        }
+        if (l == r) {
+            l++;
+            r--;
+        }
+        //左递归
+        if (left < r) {
+            doQuickSort(ary, left, r);
+        }
+        //右递归
+        if (l < right) {
+            doQuickSort(ary, l, right);
+        }
+
     }
 
     private static void shellChangeSort(int[] ary) {
@@ -34,11 +143,11 @@ public class SortDemo {
         }
         */
 
-        for (int step = ary.length/2; step > 0 ; step/=2) {
+        for (int step = ary.length / 2; step > 0; step /= 2) {
             for (int i = step; i < ary.length; i++) {
-                for (int j = i-step; j >=0; j-=step) {
-                    if(ary[j]>ary[j+step]){
-                        swap(ary,j,j+step);
+                for (int j = i - step; j >= 0; j -= step) {
+                    if (ary[j] > ary[j + step]) {
+                        swap(ary, j, j + step);
                     }
                 }
             }
@@ -46,21 +155,21 @@ public class SortDemo {
     }
 
     private static void shellInsertSort(int[] ary) {
-        for (int step = ary.length/2; step > 0 ; step/=2) {
+        for (int step = ary.length / 2; step > 0; step /= 2) {
             for (int i = step; i < ary.length; i++) {
                 int insertValue = ary[i];
                 boolean lt = false;
                 int index = 0;
-                for (int j = i-step; j >=0; j-=step) {
-                    if(ary[j]>insertValue){
-                        ary[j+step] = ary[j];
+                for (int j = i - step; j >= 0; j -= step) {
+                    if (ary[j] > insertValue) {
+                        ary[j + step] = ary[j];
                         index = j;
                         lt = true;
-                    }else{
+                    } else {
                         break;
                     }
                 }
-                if(lt){
+                if (lt) {
                     ary[index] = insertValue;
                 }
             }
@@ -84,21 +193,22 @@ public class SortDemo {
 //            }
 //        }
     }
+
     private static void insertSort(int[] ary) {
         for (int i = 1; i < ary.length; i++) {
             int insertValue = ary[i];
             boolean lt = false;
             int index = 0;
-            for (int j = i-1; j >=0 ; j--) {
-                if(ary[j]>insertValue){
-                    ary[j+1]=ary[j];
+            for (int j = i - 1; j >= 0; j--) {
+                if (ary[j] > insertValue) {
+                    ary[j + 1] = ary[j];
                     index = j;
                     lt = true;
-                }else{
+                } else {
                     break;
                 }
             }
-            if(lt) {
+            if (lt) {
                 ary[index] = insertValue;
             }
         }
@@ -106,17 +216,18 @@ public class SortDemo {
 
     /**
      * 算法时间复杂度
+     *
      * @param ary
      */
-    public static void insertSort2(int[] ary){
+    public static void insertSort2(int[] ary) {
         for (int i = 1; i < ary.length; i++) {
-            int index = i-1 ;
+            int index = i - 1;
             int insertValue = ary[i];
-            while(index >=0 && ary[index]>insertValue){
-                ary[index+1] = ary[index];
+            while (index >= 0 && ary[index] > insertValue) {
+                ary[index + 1] = ary[index];
                 index--;
             }
-            ary[index+1] = insertValue;
+            ary[index + 1] = insertValue;
         }
 
 
@@ -124,39 +235,42 @@ public class SortDemo {
 
     /**
      * 算法时间复杂度 O(n^2) 交换过程比冒泡少，比冒泡快
+     *
      * @param ary
      */
-    public static void selectSort(int[] ary){
-        for (int i = 0; i < ary.length-1; i++) {
+    public static void selectSort(int[] ary) {
+        for (int i = 0; i < ary.length - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < ary.length; j++) {
                 //第一个元素与后面相比较
-                if(ary[minIndex]>ary[j]){
+                if (ary[minIndex] > ary[j]) {
                     minIndex = j;
                 }
             }
             //发生变化才交换
-            if(minIndex!=i){
-                swap(ary,i,minIndex);
+            if (minIndex != i) {
+                swap(ary, i, minIndex);
             }
         }
 
     }
+
     /**
      * 算法时间复杂度 O(n^2)
+     *
      * @param ary
      */
     private static void bubbleSort(int[] ary) {
-        for (int i = 0; i < ary.length-1; i++) {
+        for (int i = 0; i < ary.length - 1; i++) {
             boolean changeFlag = false;
-            for (int j = 0; j < ary.length-1-i; j++) {
-                if(ary[j]>ary[j+1]){
+            for (int j = 0; j < ary.length - 1 - i; j++) {
+                if (ary[j] > ary[j + 1]) {
                     changeFlag = true;
-                    swap(ary,j,j+1);
+                    swap(ary, j, j + 1);
                 }
             }
             //如果一轮中没有交换，说明已经排好序
-            if(!changeFlag){
+            if (!changeFlag) {
                 break;
             }
         }
