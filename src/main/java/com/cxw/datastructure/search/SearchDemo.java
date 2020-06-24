186df211339ba3feb61b5eaeac7fe09782cf93a3
chengxuwei <hbyscxw@163.com>
2020-06-24 17:31:56 +0800

    排序

--- src/main/java/com/cxw/datastructure/search/SearchDemo.java
+++ src/main/java/com/cxw/datastructure/search/SearchDemo.java
@@ -1,14 +1,72 @@
 package com.cxw.datastructure.search;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SearchDemo {
     public static void main(String[] args) {
-        int[] ary = {1,1,1,1,2,3,4,5,6,7,8,9};
-        System.out.println(binarySearch(ary,1000));
+//        int[] ary = {1,1,1,1,2,3,4,5,6,7,8,9};
+        //System.out.println(binarySearch(ary,1000));
+
+        int[] ary = {1,2,3,4,5,6,7,8,9};
+        System.out.println(fbcSearch(ary,3));
+    }
+
+
+    public static int[] fibonacciArray(int maxSize){
+        int[] fbc = new int[maxSize];
+        fbc[0] = 1;
+        fbc[1] = 1;
+        for (int i = 2; i <maxSize ; i++) {
+            fbc[i] = fbc[i-1]+fbc[i-2];
+        }
+        return fbc;
+    }
+
+    public static int fbcSearch(int[] ary,int value){
+        if(ary[0]>value||ary[ary.length-1]<value){
+            return -1;
+        }
+        int low = 0;
+        int high = ary.length-1;
+        int mid = 0;
+        int k = 0;
+        int[] f = fibonacciArray(20);
+        while(high>f[k]-1){
+            k++;
+        }
+        int[] temp = new int[f[k]];
+        //0->f[k] 并将不足的位置用最大值填充  temp={1,2,3,4,5,6,7,8,9,9,9,9,9}
+        for (int i = 0; i < f[k]; i++) {
+            if(i<ary.length){
+                temp[i] = ary[i];
+            }else{
+                temp[i] = ary[ary.length-1];
+            }
+        }
+        //System.out.println(Arrays.toString(temp));
+        while(low<=high){
+            mid = low+f[k-1]-1;
+            if(temp[mid]>value){
+                //左边找 f[k-1]
+                high = mid -1;
+                k--;
+            }else if(temp[mid]<value){
+                //右边找 f[k-2]
+                low = mid+1;
+                k-=2;
+            }else {
+               if(mid<=high){
+                   return mid;
+               }else{
+                   return high;
+               }
+            }
+        }
+        return -1;
     }
 
     /**
