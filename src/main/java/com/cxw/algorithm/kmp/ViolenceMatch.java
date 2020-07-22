9939923f8a91d94ff78a16b5ba519f08d918e7b5
chengxuwei <hbyscxw@163.com>
2020-07-23 07:47:37 +0800

    violence match

---
+++ src/main/java/com/cxw/algorithm/kmp/ViolenceMatch.java
@@ -0,0 +1,30 @@
+package com.cxw.algorithm.kmp;
+
+public class ViolenceMatch {
+    public static void main(String[] args) {
+        String str1 = "12314123";
+        String str2 = "231";
+        int index = violenceMatch(str1,str2);
+        System.out.println(index);
+    }
+
+    private static int violenceMatch(String str1, String str2) {
+        char[] ary1 = str1.toCharArray();
+        char[] ary2 = str2.toCharArray();
+        int i = 0;
+        int j = 0;
+        while(i<ary1.length&&j<ary2.length){
+            if(ary1[i]==ary2[j]){
+                i++;
+                j++;
+            }else{
+                i = i-j+1;
+                j=0;
+            }
+        }
+        if(j==ary2.length){
+            return i-j;
+        }
+        return -1;
+    }
+}
